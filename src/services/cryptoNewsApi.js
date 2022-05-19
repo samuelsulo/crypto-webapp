3a514fb28974d1d0121885dd8aaeb46faf8f3d0b
Samuel Sulo <samuelsulo0802@gmail.com>
2022-05-19 23:57:08 +0200

    feat(news): add news page

---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,25 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoNewsHeaders = {
+  'X-BingApis-SDK': 'true',
+  'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com',
+  'X-RapidAPI-Key': '3f2623f7ecmshb18583b478e81d0p148883jsn646146e46b61'
+};
+
+const baseUrl = 'https://bing-news-search1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
+
+export const cryptoNewsApi = createApi({
+  reducerPath: 'cryptoNewsApi',
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptoNews: builder.query({
+      query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`)
+    })
+  })
+});
+
+export const {
+  useGetCryptoNewsQuery
+} = cryptoNewsApi;
