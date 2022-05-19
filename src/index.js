33ca9f052d9546eb2ef989dc66830215defeb5af
Samuel Sulo <81391401+Samuel-Sulo@users.noreply.github.com>
2022-05-19 22:54:06 +0200

    feature(pages): add pages home and cryptocurrencies (#2)

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import App from './App.js';
+import store from './app/store';
 import 'antd/dist/antd.css';
 
 ReactDOM
   .createRoot(document.getElementById('root'))
   .render(
     <Router>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Router>
   );
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+  'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
+  'X-RapidAPI-Key': '3f2623f7ecmshb18583b478e81d0p148883jsn646146e46b61'
+};
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: 'cryptoApi',
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: (count) => createRequest(`/coins?limit=${count}`)
+    })
+  })
+});
+
+export const {
+  useGetCryptosQuery
+} = cryptoApi;
